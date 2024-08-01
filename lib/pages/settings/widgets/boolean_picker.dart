3f7466435ed08f2c353003e066132e4e49f942de
JasonBoyett <jason.boyett@gmail.com>
2024-08-01 02:14:46 -0500

    re-structured components for readability

--- lib/pages/settings/widgets/boolean_picker.dart
+++ lib/pages/settings/widgets/boolean_picker.dart
@@ -11,44 +11,69 @@ Widget booleanPicker({
 }) {
   isTablet ??= false;
   return isTablet
-      ? Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    text,
-                    style: TextStyle(
-                      fontStyle: GoogleFonts.kumbhSans().fontStyle,
-                      fontWeight: FontWeight.bold,
-                      color: PomodoroUI.textMidDark,
-                    ),
-                  ),
-                  Switch(
-                    value: value,
-                    onChanged: onToggle,
-                    activeColor: color,
-                  ),
-                ],
-              ),
-            ),
-          ],
+      ? _tabletPicker(
+          onToggle: onToggle,
+          value: value,
+          text: text,
+          color: color,
         )
-      : Column(
+      : _mobilePicker(
+          onToggle: onToggle,
+          value: value,
+          text: text,
+          color: color,
+        );
+}
+
+Widget _mobilePicker({
+  required void Function(bool) onToggle,
+  required bool value,
+  required String text,
+  required Color color,
+}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      SizedBox(
+        width: 150,
+        child: Text(
+          text,
+          style: TextStyle(
+            fontStyle: GoogleFonts.kumbhSans().fontStyle,
+            fontWeight: FontWeight.bold,
+            color: PomodoroUI.textMidDark,
+          ),
+        ),
+      ),
+      Switch(
+        value: value,
+        onChanged: onToggle,
+        activeColor: color,
+      ),
+    ],
+  );
+}
+
+Widget _tabletPicker({
+  required void Function(bool) onToggle,
+  required bool value,
+  required String text,
+  required Color color,
+}) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: [
+      Padding(
+        padding: const EdgeInsets.only(left: 10.0),
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
-              width: 150,
-              child: Text(
-                text,
-                style: TextStyle(
-                  fontStyle: GoogleFonts.kumbhSans().fontStyle,
-                  fontWeight: FontWeight.bold,
-                  color: PomodoroUI.textMidDark,
-                ),
+            Text(
+              text,
+              style: TextStyle(
+                fontStyle: GoogleFonts.kumbhSans().fontStyle,
+                fontWeight: FontWeight.bold,
+                color: PomodoroUI.textMidDark,
               ),
             ),
             Switch(
@@ -57,5 +82,8 @@ Widget booleanPicker({
               activeColor: color,
             ),
           ],
-        );
+        ),
+      ),
+    ],
+  );
 }
